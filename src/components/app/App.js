d1899b6cfb3fe54e61e9b032e4a7df417d67afe2
tamara_hera <tamara.herashchenko@gmail.com>
2023-09-19 13:55:24 +0200

    add localStorage

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,46 +10,32 @@ import Footer from '../footer/Footer';
 
 import './app.scss';
 
+const LOCAL_STORAGE_KEY = 'shopList.data';
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [
-        {
-          name: 'Bananas',
-          amount: 2,
-          price: 3,
-          checked: false,
-          important: false,
-          id: uuidv4()
-        },
-        {
-          name: 'Cat`s food',
-          amount: 5,
-          price: 10,
-          checked: false,
-          important: true,
-          id: uuidv4()
-        },
-        {
-          name: 'Bread',
-          amount: 4,
-          price: 8,
-          checked: false,
-          important: false,
-          id: uuidv4()
-        },
-        {
-          name: 'Milk',
-          amount: 3,
-          price: 6,
-          checked: true,
-          important: false,
-          id: uuidv4()
-        }
-      ],
+      data: [],
       filterValue: 'all',
       searchValue: ''
+    }
+  }
+
+  componentDidMount() {
+    const localData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    console.log(localData)
+    this.setState({
+      data: localData.data,
+      filterValue: localData.filterValue,
+      searchValue: ''
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState !== this.state) {
+      const jsonObj = JSON.stringify(this.state);
+      localStorage.setItem(LOCAL_STORAGE_KEY, jsonObj); // set localStorage
     }
   }
 
